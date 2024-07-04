fbbc5f10f88d70a91b86cb2367572df9b535ab9d
Osama Jaradat <38565843+Oso00Luffy@users.noreply.github.com>
2024-07-04 12:57:34 +0300

    Create FancyCard.js

---
+++ src/components/FancyCard.js
@@ -0,0 +1,18 @@
+// src/components/FancyCard.js
+
+import React from 'react';
+import '../css/FancyCard.css'; // Import the CSS file
+
+const FancyCard = ({ imageSrc, title, description }) => {
+  return (
+    <div className="card-container">
+      <img className="card-image" src={imageSrc} alt="Background" />
+      <div className="card-content">
+        <h2 className="card-title">{title}</h2>
+        <p className="card-description">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default FancyCard;
